dd48be817dcf42cdfbd7f272b1cee38f5e865b51
numpad_7 <87062000+Luamnoxu@users.noreply.github.com>
2024-04-09 07:32:04 +0200

    PYTHONMAN can now write Files for Screens that already exist

---
+++ gimmick/screens/ScreenOptions.lua
@@ -0,0 +1,13 @@
+
+return {
+    Init = function(self) Trace('theme.com') end,
+    overlay = gimmick.ActorScreen(function(self, ctx)
+    end),
+    underlay = gimmick.ActorScreen(function(self, ctx)
+    end),
+    header = gimmick.ActorScreen(function(self, ctx)
+    end),
+    footer = gimmick.ActorScreen(function(self, ctx)
+    end),
+}
+        
