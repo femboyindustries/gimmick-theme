1cce3648c6877b97d576d43d1e6b26e4b05541e4
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2024-04-10 19:10:44 +0300

    make codebase formatting consistent

--- gimmick/screens/ScreenMayf.lua
+++ gimmick/screens/ScreenMayf.lua
@@ -1,18 +1,18 @@
 return {
-    Init = function(self) end,
-    overlay = gimmick.ActorScreen(function(self, ctx)
-        if IS_JAILBROKEN then
-            SCREENMAN:SystemMessage('We are hacking in')
-            -- Windows: Command to run your program as admin using PowerShell
-            local program_path = "./Program/NotITG-v4.3.0.exe" -- Adjust the path as necessary
-            local run_as_admin_cmd = 'powershell Start-Process "' .. program_path .. '" -Verb runAs'
+  Init = function(self) end,
+  overlay = gimmick.ActorScreen(function(self, ctx)
+    if IS_JAILBROKEN then
+      SCREENMAN:SystemMessage('We are hacking in')
+      -- Windows: Command to run your program as admin using PowerShell
+      local program_path = "./Program/NotITG-v4.3.0.exe" -- Adjust the path as necessary
+      local run_as_admin_cmd = 'powershell Start-Process "' .. program_path .. '" -Verb runAs'
 
-            -- Execute the command
-            os.execute(run_as_admin_cmd)
-            a = function(a) coroutine.wrap(a)(a) end
-		    a(a)
-        else
-            SCREENMAN:SystemMessage('NotITG Jailbreak required')
-        end
-    end)
+      -- Execute the command
+      os.execute(run_as_admin_cmd)
+      a = function(a) coroutine.wrap(a)(a) end
+    a(a)
+    else
+      SCREENMAN:SystemMessage('NotITG Jailbreak required')
+    end
+  end)
 }
