ffadedba3bf19882b3bee4bcc3b2ad88bf394e0c
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2024-04-12 22:31:10 +0300

    kills you

--- gimmick/screens/ScreenOptions.lua
+++ gimmick/screens/ScreenOptions.lua
@@ -1,36 +1,36 @@
 
 return {
-    Init = function(self)
-      if not gimmick.getSaved() then
-        gimmick.saveInit()
-      end
-    end,
-    overlay = gimmick.ActorScreen(function(self, ctx)
-    end),
-    underlay = gimmick.ActorScreen(function(self, ctx)
-      local bg = ctx:Sprite('Graphics/_missing')
-      bg:scaletocover(0,0,SCREEN_WIDTH,SCREEN_HEIGHT)
-    end),
-    header = gimmick.ActorScreen(function(self, ctx)
-    end),
-    footer = gimmick.ActorScreen(function(self, ctx)
-    end),
+  Init = function(self)
+    if not gimmick.getSaved() then
+      gimmick.saveInit()
+    end
+  end,
+  overlay = gimmick.ActorScreen(function(self, ctx)
+  end),
+  underlay = gimmick.ActorScreen(function(self, ctx)
+    local bg = ctx:Sprite('Graphics/_missing')
+    bg:scaletocover(0,0,SCREEN_WIDTH,SCREEN_HEIGHT)
+  end),
+  header = gimmick.ActorScreen(function(self, ctx)
+  end),
+  footer = gimmick.ActorScreen(function(self, ctx)
+  end),
 
-    --- El soyjak
-    Line1 = function()
-        local t = gimmick.OptionRowBase('Console (Ctrl+9)')
+  --- El soyjak
+  Line1 = function()
+    local t = gimmick.OptionRowBase('Console (Ctrl+9)')
 
-        t.Choices = {'ON', 'OFF'}
-        t.LayoutType = 'ShowAllInRow'
-        t.LoadSelections = function(self,list) 
-          local option = search(t.Choices,gimmick.getSavedOption('Console')) or 1 
-          list[option] = true 
-        end
-        t.SaveSelections = function(self,list)
-          gimmick.ezSave('Console',t.Choices[search(list,true)] or 1)
-        end
+    t.Choices = {'ON', 'OFF'}
+    t.LayoutType = 'ShowAllInRow'
+    t.LoadSelections = function(self,list) 
+      local option = search(t.Choices,gimmick.getSavedOption('Console')) or 1 
+      list[option] = true 
+    end
+    t.SaveSelections = function(self,list)
+      gimmick.ezSave('Console',t.Choices[search(list,true)] or 1)
+    end
 
-        return t
+    return t
 	end
 }
         
