22dc5ed82abeb78385b22a283d8e498e7f709803
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2024-08-04 11:12:39 +0300

    fix double indent

--- gimmick/screens/ScreenSelectMascot.lua
+++ gimmick/screens/ScreenSelectMascot.lua
@@ -10,7 +10,7 @@ local cursor = easable(1, 28)
 local mascotPaths = {}
 local mascotNames = mascots.getMascots()
 for _, mascot in ipairs(mascotNames) do
-    mascotPaths[#mascotPaths + 1] = mascots.getPaths(mascot)
+  mascotPaths[#mascotPaths + 1] = mascots.getPaths(mascot)
 end
 
 -- Register the input event handling outside of the returned table
@@ -19,100 +19,100 @@ local menuRightCooldown = 0.05
 
 -- Define the function to handle input events, you can register it outside the table
 local function handleInputEvents(pn, button)
-    if SCREENMAN:GetTopScreen():GetName() == 'ScreenSelectMascot' then
-        local currentTime = os.clock()
-        if button == 'MenuRight' or button == 'MenuLeft' then
-            if currentTime - lastMenuRightPress > menuRightCooldown then
-                if button == 'MenuRight' then
-                    selected = (selected % #mascotNames) + 1
-                    active_i = active_i + 1
-                elseif button == 'MenuLeft' then
-                    selected = selected - 1
-                    if selected < 1 then
-                        selected = #mascotNames
-                    end
-                    active_i = active_i - 1
-                end
-                lastMenuRightPress = currentTime
-                cursor:set(active_i)
-            end
-        elseif button == 'Start' then
-            print('gay people tomorrow 10am')
-            save.data.settings.mascot = mascotNames[selected]
-            save.save()
+  if SCREENMAN:GetTopScreen():GetName() == 'ScreenSelectMascot' then
+    local currentTime = os.clock()
+    if button == 'MenuRight' or button == 'MenuLeft' then
+      if currentTime - lastMenuRightPress > menuRightCooldown then
+        if button == 'MenuRight' then
+          selected = (selected % #mascotNames) + 1
+          active_i = active_i + 1
+        elseif button == 'MenuLeft' then
+          selected = selected - 1
+          if selected < 1 then
+            selected = #mascotNames
+          end
+          active_i = active_i - 1
         end
+        lastMenuRightPress = currentTime
+        cursor:set(active_i)
+      end
+    elseif button == 'Start' then
+      print('gay people tomorrow 10am')
+      save.data.settings.mascot = mascotNames[selected]
+      save.save()
     end
+  end
 end
 
 event.on('press', handleInputEvents) -- Register the event handler
 
 return {
-    PrevScreen = "ScreenSelectOptions",
-    Init = function(self)
-    end,
+  PrevScreen = "ScreenSelectOptions",
+  Init = function(self)
+  end,
 
-    overlay = gimmick.ActorScreen(function(self, ctx)
-        local mascot_actors = {}
-        local background_actors = {}
-        local bmt = TextPool.new(ctx, FONTS.sans_serif, nil, function(actor)
-            actor:xy(scx * 1.5, scy * 0.5)
-            actor:zoom(0.3)
-        end)
-
-        for index, value in ipairs(mascotNames) do
-            local actor = ctx:Sprite(mascotPaths[index]['character'])
-            actor:scaletofit(0, 0, sw * 0.5, sh * 0.5)
-            actor:xy(scx * 0.6, scy)
-            actor:ztest(1)
-            actor:zbuffer(1)
-            table.insert(mascot_actors, actor)
-
-            local background = ctx:Sprite(mascotPaths[index]['background'])
-            background:scaletocover(0, 0, sw, sh)
-            table.insert(background_actors, background)
-        end
-
-        local description = bmt:get(mascots.getDescription(mascotNames[selected]))
-        local name = bmt:get(mascotNames[selected])
-        local disclaimer = bmt:get('Press Enter to save your selection!')
-
-        disclaimer:zoom(0.7)
-        disclaimer:xy(scx,scy*0.2)
-
-        name:zoom(1)
-        name:xy(scx * 0.8, sh * 0.9)
-
-        self:fov(70)
-        local darken = ctx:Quad()
-        darken:diffuse(0, 0, 0, 0.2)
-        darken:stretchto(0, 0, sw, sh)
-
-        local angle_step = (2 * math.pi) / #mascot_actors
-
-        setDrawFunctionWithDT(self, function(dt)
-          -- Update background and darken effect
-          background_actors[selected]:Draw()
-          darken:Draw()
-
-          
-          description:settext(mascots.getDescription(mascotNames[selected]))
-          name:settext(mascotNames[selected])
-
-          cursor:update(dt)
-
-          local offset = math.pi / 2 - ((selected - active_i) * angle_step)
-          for i, mascot in ipairs(mascot_actors) do
-              local a = ((i - cursor.eased) * angle_step) + offset
-              a = a % (2 * math.pi)
-              local x, y = math.cos(a), math.sin(a)
-              mascot:diffusealpha(selected ~= i and 0.2 or 1)
-              mascot:x((x * (sw * 0.3)) + scx * 0.8)
-              mascot:z((y * (sh * 0.3)))
-              mascot:Draw()
-          end
-          disclaimer:Draw()
-          description:Draw()
-          name:Draw()
-        end)
+  overlay = gimmick.ActorScreen(function(self, ctx)
+    local mascot_actors = {}
+    local background_actors = {}
+    local bmt = TextPool.new(ctx, FONTS.sans_serif, nil, function(actor)
+      actor:xy(scx * 1.5, scy * 0.5)
+      actor:zoom(0.3)
     end)
+
+    for index, value in ipairs(mascotNames) do
+      local actor = ctx:Sprite(mascotPaths[index]['character'])
+      actor:scaletofit(0, 0, sw * 0.5, sh * 0.5)
+      actor:xy(scx * 0.6, scy)
+      actor:ztest(1)
+      actor:zbuffer(1)
+      table.insert(mascot_actors, actor)
+
+      local background = ctx:Sprite(mascotPaths[index]['background'])
+      background:scaletocover(0, 0, sw, sh)
+      table.insert(background_actors, background)
+    end
+
+    local description = bmt:get(mascots.getDescription(mascotNames[selected]))
+    local name = bmt:get(mascotNames[selected])
+    local disclaimer = bmt:get('Press Enter to save your selection!')
+
+    disclaimer:zoom(0.7)
+    disclaimer:xy(scx,scy*0.2)
+
+    name:zoom(1)
+    name:xy(scx * 0.8, sh * 0.9)
+
+    self:fov(70)
+    local darken = ctx:Quad()
+    darken:diffuse(0, 0, 0, 0.2)
+    darken:stretchto(0, 0, sw, sh)
+
+    local angle_step = (2 * math.pi) / #mascot_actors
+
+    setDrawFunctionWithDT(self, function(dt)
+      -- Update background and darken effect
+      background_actors[selected]:Draw()
+      darken:Draw()
+
+      
+      description:settext(mascots.getDescription(mascotNames[selected]))
+      name:settext(mascotNames[selected])
+
+      cursor:update(dt)
+
+      local offset = math.pi / 2 - ((selected - active_i) * angle_step)
+      for i, mascot in ipairs(mascot_actors) do
+        local a = ((i - cursor.eased) * angle_step) + offset
+        a = a % (2 * math.pi)
+        local x, y = math.cos(a), math.sin(a)
+        mascot:diffusealpha(selected ~= i and 0.2 or 1)
+        mascot:x((x * (sw * 0.3)) + scx * 0.8)
+        mascot:z((y * (sh * 0.3)))
+        mascot:Draw()
+      end
+      disclaimer:Draw()
+      description:Draw()
+      name:Draw()
+    end)
+  end)
 }
